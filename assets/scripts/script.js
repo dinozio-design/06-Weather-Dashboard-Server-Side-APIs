c1ae3efb576d6694624b7abee459b7133c81f5b5
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-28 13:20:02 -0400

    started the script with variables to map to html elements

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,7 @@
 var opernWeatherKey = "dec5aeffcb14cae3dce591a4fba4758b";
+var userFormEl = document.querySelector('#userForm');
+var historyButtonsEl = document.querySelector('#historyButtons');
+// var nameInputEl = document.querySelector('#username');
+var cityNameSearchTerm = document.querySelector('#cityNameSearch');
+var todaysDate = document.querySelector('#todaysDate');
+var resultContainerEl = document.querySelector('#resultContainer');
