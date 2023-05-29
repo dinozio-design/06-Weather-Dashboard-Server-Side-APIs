9ed402cb6794dcb94583c2fdc681771c77c03ff8
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-29 16:07:47 -0400

    country selection works with dymanic html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,5 +3,20 @@ var userFormEl = $('#userForm');
 var historyButtonsEl = $('#historyButtons');
 // var nameInputEl = document.querySelector('#username');
 var cityNameSearchTerm = $('#cityNameSearch');
+var countrySelect = $('#countrySelect')
 var todaysDate = $('#todaysDate');
 var resultContainerEl = $('#resultContainer');
+
+var countryCodes = [["CAN","Canada"],["US","United States"]];
+//     CAN:"Canada",
+//     US: "United States"
+
+// };
+
+$.each(countryCodes, function(){
+    console.log(this[0], this[1]);
+    countrySelect.append(`<option id="${this[0]}">${this[1]}</option>`)
+})
+
+// console.log(countryCodes);
+// console.log(countrySelect);
