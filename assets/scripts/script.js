47500ef6a1974d29b64b566bd70ddff4ce255e1c
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-29 15:20:13 -0400

    updated html with Bulma classes for style added jQuery link and started woking on script with js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 var opernWeatherKey = "dec5aeffcb14cae3dce591a4fba4758b";
-var userFormEl = document.querySelector('#userForm');
-var historyButtonsEl = document.querySelector('#historyButtons');
+var userFormEl = $('#userForm');
+var historyButtonsEl = $('#historyButtons');
 // var nameInputEl = document.querySelector('#username');
-var cityNameSearchTerm = document.querySelector('#cityNameSearch');
-var todaysDate = document.querySelector('#todaysDate');
-var resultContainerEl = document.querySelector('#resultContainer');
+var cityNameSearchTerm = $('#cityNameSearch');
+var todaysDate = $('#todaysDate');
+var resultContainerEl = $('#resultContainer');
