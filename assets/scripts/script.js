5194ad86cda800e7224aa7c42eee23ca08e68b58
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-29 17:32:14 -0400

    CODE WORKS FOR NOW next is to add Modals, it may break the code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,18 +2,19 @@ var opernWeatherKey = "dec5aeffcb14cae3dce591a4fba4758b";
 var userFormEl = $('#userForm');
 var historyButtonsEl = $('#historyButtons');
 // var nameInputEl = document.querySelector('#username');
-var cityNameSearchTerm = $('#cityNameSearch');
+var cityNameSearch = $('#cityNameSearch');
+var cityNameDisplay = $('#cityNameDisplay');
 var countrySelect = $('#countrySelect')
 var todaysDate = $('#todaysDate');
 var resultContainerEl = $('#resultContainer');
 
-var countryCodes = [["CAN","Canada"],["US","United States"]];
+var countryCodes = [["CAN", "Canada"], ["US", "United States"]];
 //     CAN:"Canada",
 //     US: "United States"
 
 // };
 
-$.each(countryCodes, function(){
+$.each(countryCodes, function () {
     console.log(this[0], this[1]);
     countrySelect.append(`<option id="${this[0]}">${this[1]}</option>`)
 })
@@ -21,9 +22,29 @@ $.each(countryCodes, function(){
 // console.log(countryCodes);
 // console.log(countrySelect);
 
-function userSearch(event){
-    event.preventDefault();
-    console.log(`was pushed`, event);
-}
 
-userFormEl.on('submit',userSearch);
+function submitHandler(event) {
+    event.preventDefault();
+    var cityName = this.cityNameSearch.value.trim();
+    if (cityName) {
+        // getUserRepos(cityName);
+        console.log(`I got ${cityName}`);
+
+        cityNameSearch.textContent = '';
+        countrySelect.value = 'Select Country';
+    } else {
+        alert('Please enter a GitHub username');
+       
+    }
+};
+// console.log(`was pushed`, event, this);
+
+
+userFormEl.on('submit', submitHandler);
+
+
+// js-example
+
+// function ($el){
+//     $el.classList.add('s-active');
+// }
