fb60e61935eb4362884a29df8d11ecaa4dd16672
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-31 16:15:16 -0400

    wrote fetch functions to get API data from openweather

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,6 @@
-var opernWeatherKey = "dec5aeffcb14cae3dce591a4fba4758b";
+var opernWeatherKey = "71786d75fbc7f8c6556506473f4a9371";
 var userFormEl = $('#userForm');
 var historyButtonsEl = $('#historyButtons');
-// var nameInputEl = document.querySelector('#username');
 var cityNameSearch = $('#cityNameSearch');
 var cityNameDisplay = $('#cityNameDisplay');
 var countrySelect = $('#countrySelect')
@@ -15,12 +14,64 @@ var countryCodes = [["CAN", "Canada"], ["US", "United States"]];
 // };
 
 $.each(countryCodes, function () {
-    console.log(this[0], this[1]);
+    // console.log(this[0], this[1]);
     countrySelect.append(`<option id="${this[0]}">${this[1]}</option>`)
 })
 
 // console.log(countryCodes);
 // console.log(countrySelect);
+
+function getWeather(lat, lon) {
+    console.log(lat);
+    console.log(lon);
+    var apiURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&cnt=3&appid=" + opernWeatherKey +"&units=metric";
+    console.log(apiURL);
+    fetch(apiURL)
+    .then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                // getWeather(data[0].lat, data[0].lon);
+    console.log(data);
+
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function (error) {
+        alert('Unable to connect to GitHub');
+    });
+    
+}
+
+function getGeoCode(city, country) {
+    var code = "";
+    if (country = "Canada") {
+        code = "CA";
+    } else if (country = "United States") {
+        code = "US";
+    }
+
+    // api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=71786d75fbc7f8c6556506473f4a9371
+    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + code + "&appid=" + opernWeatherKey;
+
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    getWeather(data[0].lat, data[0].lon);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to GitHub');
+        });
+
+}
 
 
 function submitHandler(event) {
@@ -29,25 +80,18 @@ function submitHandler(event) {
     var countryName = this.countrySelect.value;
     // console.log(countryName);
     if (cityName && countryName != "Select Country") {
-        // getUserRepos(cityName);
-        console.log(`I got ${cityName}`);
-        console.log(`I got ${countryName}`);
-        
+
+        // console.log(`I got ${cityName}`);
+        // console.log(`I got ${countryName}`);
+
         cityNameSearch.textContent = '';
         countrySelect.value = 'Select Country';
     } else {
         alert('Please enter valid city and country name');
-       
     }
 };
 // console.log(`was pushed`, event, this);
 
+getGeoCode("Toronto", "Canada");
 
 userFormEl.on('submit', submitHandler);
-
-
-// js-example
-
-// function ($el){
-//     $el.classList.add('s-active');
-// }
